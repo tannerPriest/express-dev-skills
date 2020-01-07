f4f053f835b808a3202aa92237eb718c06df947b
killerwhiteshark <killerwhiteshark@users.noreply.github.com>
2020-01-07 12:02:41 -0800

    Make model folder and make content

---
+++ models/skill.js
@@ -0,0 +1,15 @@
+const skills = [
+    {skill: "HTML", level: "Proficient"},
+    {skill:"CSS" , level: "Proficent"},
+    {skill:"Javascript" , level:"Questionable"},
+    {skill:"Node JS" , level:"Whats Node JS?"},
+    {skill:"Express" , level:"No Comment"}
+];
+
+const getSkill = (skill) => {
+    return skills;
+}
+
+module.exports {
+    getSkill
+}
